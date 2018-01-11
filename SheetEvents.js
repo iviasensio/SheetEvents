d2369667839f7cff464407eb2d8004b41f4607ec
Ivan Felipe Asensio <ivan.felipe@qlik.com>
2018-01-11 17:32:10 +0100

    Time delay in first access and improve loop actions

--- SheetEvents.js
+++ SheetEvents.js
@@ -42,8 +42,10 @@ define( [
 				
 				if(_self){
 					var app = qlik.currApp();
-					
-					var del = 0;
+										
+					var counter = 0;
+					var ii = 0;
+					var del = $scope.layout.props['delay'];
 					var fld = null;
 					var val = null;
 					var val2 = null;
@@ -51,9 +53,8 @@ define( [
 					var vari = null;
 					tasksArray = [];
 										
-					for ( var i = 1; i <= 10; i++ ) {
-						
-						del = $scope.layout.props['delay'];
+					for (var  i = 1; i <= 10; i++ ) {
+						ii = i - 1;						
 						fld = "'" + $scope.layout.props['field' + i] + "'";
 						val = "'" + $scope.layout.props['value' + i] + "'";
 						val2 = $scope.layout.props['value' + i].split(';').join("'};{qText: '");
@@ -116,8 +117,8 @@ define( [
 								break;
 							case "applyBookmark":
 								if ( !_.isEmpty( _self.backendApi.model.layout.props['bookmark' + i] ) ) {
-									var myBookmark = app.bookmark.apply( _self.backendApi.model.layout.props['bookmark' + i] );
-									tasksArray[i - 1] = 'app.bookmark.apply( ' + myBookmark + ' )';
+									var myBookmark = "'" + _self.backendApi.model.layout.props['bookmark' + i] + "'";
+									tasksArray[i - 1] = 'app.bookmark.apply( ' + myBookmark + ' )';									
 								}
 								break;
 							case "setVariable":								
@@ -125,36 +126,31 @@ define( [
 									tasksArray[i - 1] = 'app.variable.setContent(' + vari + ',' + val + ')';
 								}
 								break;
+							case "none":								
+								i = 10;
+								break;								
 							default:
+								i = 10;
 								break;
 						}
 						
 					}
 					
-					setTimeout(function(){ eval(tasksArray[0])
-						.then(setTimeout(function(){ eval(tasksArray[1])
-							.then(setTimeout(function(){ eval(tasksArray[2])
-								.then(setTimeout(function(){ eval(tasksArray[3])
-									.then(setTimeout(function(){ eval(tasksArray[4])
-										.then(setTimeout(function(){ eval(tasksArray[5])
-											.then(setTimeout(function(){ eval(tasksArray[6])
-												.then(setTimeout(function(){ eval(tasksArray[7])
-													.then(setTimeout(function(){ eval(tasksArray[8])
-														.then(eval(tasksArray[9]))														
-													}, del))
-												}, del))
-											}, del))
-										}, del))
-									}, del))
-								}, del))
-							}, del))
-						}, del))
-					},del)
-					
+					start(0);
+					function start(counter){
+						if(counter < ii){
+						    setTimeout(function(){						    	
+						    	eval(tasksArray[counter]);
+						      	counter++;
+						      	start(counter);
+						    }, del);
+						}
+					};			
 				}
 
 			}],
 			paint : function($element, layout) {
+				console.log(comptar);
 				_self = this;
 				//this execution must be executed the first time you access to the sheet during a session o after an F5
 				// the _self variable will be = null
@@ -162,86 +158,107 @@ define( [
 				if (comptar == 0) {
 					comptar++;
 					var app = qlik.currApp();
-
+					
+					var counter = 0;
+					var ii = 0;
+					var del = _self.backendApi.model.layout.props['delay'];
 					var fld = null;
 					var val = null;
+					var val2 = null;
 					var softlock = null;
 					var vari = null;
 
 					for ( var i = 1; i <= 10; i++ ) {
-
+						ii = i - 1;
 						fld = "'" + _self.backendApi.model.layout.props['field' + i] + "'";
 						val = _self.backendApi.model.layout.props['value' + i];
+						val2 = _self.backendApi.model.layout.props['value' + i].split(';').join("'};{qText: '");
 						softlock = _self.backendApi.model.layout.props['softlock' + i];
 						vari = _self.backendApi.model.layout.props['variable' + i];
 
 						switch ( _self.backendApi.model.layout.props['actionBefore' + i] ) {
 							case "clearAll":
-								app.clearAll();
+								tasksArray[i - 1] = 'app.clearAll()';
 								break;
 							case "lockAll":
-								app.lockAll();
+								tasksArray[i - 1] = 'app.lockAll()';
 								break;
 							case "unlockAll":
-								app.unlockAll();
+								tasksArray[i - 1] = 'app.unlockAll()';								
 								break;
 							case "clearField":
 								if ( !_.isEmpty( fld ) ) {
-									app.field( fld ).clear();
+									tasksArray[i - 1] = 'app.field( ' + fld + ' ).clear()';
 								}
 								break;
 							case "selectAlternative":
 								if ( !_.isEmpty( fld ) ) {
-									app.field( fld ).selectAlternative( softlock );
+									tasksArray[i - 1] = 'app.field( ' + fld + ' ).selectAlternative( ' + softlock + ' )';
 								}
 								break;
 							case "selectExcluded":
 								if ( !_.isEmpty( fld ) ) {
-									app.field( fld ).selectExcluded( softlock );
+									tasksArray[i - 1] = 'app.field( ' + fld + ' ).selectExcluded( ' + softlock + ')';
 								}
 								break;
 							case "selectPossible":
 								if ( !_.isEmpty( fld ) ) {
-									app.field( fld ).selectPossible( softlock );
+									//tasksArray[i - 1] = 'app.field( ' + fld + ' ).selectPossible( ' + softlock + ')';
+									tasksArray[i - 1] = 'app.field( ' + fld + ' ).selectPossible()';
 								}
 								break;
 							case "selectField":
+								
 								if ( !_.isEmpty( fld ) && ( !_.isEmpty( val )) ) {
-									app.field( fld ).selectMatch( val, false );
+									tasksArray[i - 1] = "app.field(" + fld + ").selectMatch('" + val + "', true )";
 								}
+								
 								break;
 							case "selectValues":
 								if ( !_.isEmpty( fld ) && ( !_.isEmpty( val )) ) {
-									var vals = splitToStringNum( val, ';' );
-									app.field( fld ).selectValues( vals, false );
-									console.log('1:'+'app.field( ' + fld + ' ).selectValues( ' + vals + ', false )');
+									var vals = '[' + splitToStringNum( "{qText: '" + val2 + "'}", ';' ) + ']';
+									tasksArray[i - 1] = 'app.field( ' + fld + ' ).selectValues( ' + vals + ', true,true )';																		
 								}
 								break;
 							case "selectandLockField":
 								if ( !_.isEmpty( fld ) && ( !_.isEmpty( val )) ) {
-									app.field( fld ).selectMatch( val, true );
-									app.field( fld ).lock()
+									tasksArray[i - 1] = 'app.field( ' + fld + ' ).selectMatch( ' + val + ', true );app.field( ' + fld + ' ).lock()';
 								}
 								break;
 							case "lockField":
 								if ( !_.isEmpty( fld ) ) {
-									app.field( fld ).lock()
+									tasksArray[i - 1] = 'app.field( ' + fld + ' ).lock()';
 								}
 								break;
 							case "applyBookmark":
 								if ( !_.isEmpty( _self.backendApi.model.layout.props['bookmark' + i] ) ) {
-									app.bookmark.apply( _self.backendApi.model.layout.props['bookmark' + i] );
+									var myBookmark = "'" + _self.backendApi.model.layout.props['bookmark' + i] + "'";
+									tasksArray[i - 1] = 'app.bookmark.apply( ' + myBookmark + ' )';									
 								}
 								break;
-							case "setVariable":
-								if ( !_.isEmpty(vari) ) {
-									app.variable.setContent(vari, val);
+							case "setVariable":								
+								if ( !_.isEmpty(vari) ) {									
+									tasksArray[i - 1] = 'app.variable.setContent(' + vari + ',' + val + ')';
 								}
 								break;
+							case "none":								
+								i = 10;
+								break;								
 							default:
+								i = 10;
 								break;
-						}
+						}						
 					}
+					start(0);
+					function start(counter){
+						if(counter < ii){
+						    setTimeout(function(){						    	
+						    	eval(tasksArray[counter]);
+						      	counter++;
+						      	start(counter);
+						    }, del);
+						}
+					};	
 				}								
 			}
 		};
